861b5aa9117b7692fb6371bef7bb8082d93058fd
Mattaclp <51333346+Mattaclp@users.noreply.github.com>
2026-02-06 09:30:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,12 +10,19 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# change feeds.conf.default
+rm -rf feeds.conf.default
+wget https://github.com/Mattaclp/NewLEDE/raw/refs/heads/main/1806feeds.conf.default/feeds.conf.default
 
-# Add a feed source
-sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
-sed -i '2i src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
+# add passwall
+sed -i '$a src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '$a src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' feeds.conf.default
 
-#sed -i '2s/^#//' feeds.conf.default
-#sed -i '3s/^/#/' feeds.conf.default
+# add helloworld
+#git clone https://github.com/fw876/helloworld.git package/helloworld
+sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git;master' feeds.conf.default
+
+git clone -b dev https://github.com/vernesong/OpenClash.git package/lean/OpenClash
+mv package/lean/OpenClash/luci-app-openclash package/lean/
+rm -rf package/lean/OpenClash
+
